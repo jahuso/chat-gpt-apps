86450f6c5561d9f69041be9463b1c3a6b278c293
Jose Ahumada <jose.ahumada@amadeus.com>
2023-06-22 02:24:15 -0500

    provider changed

--- index.js
+++ index.js
@@ -1,132 +1,44 @@
 const express = require("express");
+const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 require("dotenv").config();
-const {Configuration, OpenAIApi} = require("openai");
+const endpoint = process.env["AZURE_OPENAI_ENDPOINT"] ;
+const azureApiKey = process.env["AZURE_OPENAI_KEY"] ;
+const deploymentId = process.env["DEPLOYMENT_ID"] ;
 
-const app = express();
+const prompt = ["What is Azure OpenAI?"];
 
-app.use(express.json());
+async function main() {
+  console.log("== Get completions Sample ==");
 
-const configuration = new Configuration({
-    apiKey: process.env.OPEN_AI_KEY,
-});
-const openai = new OpenAIApi(configuration);
+  const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
+  //const {choices} = await client.getCompletions(deploymentId, prompt);
 
-app.get('/', (req, res) => {
-    res.json({msg: 'Hello world!!!'});
-});
+  const response = await client.getCompletions(deploymentId,prompt);
 
-app.post("/emoji-to-movie", async(req,res)=>{
-    try {
-        const{movie} = req.body;
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: "Convert emojis to movie titles.\n\n" + `${movie}` +":",
-            //prompt: "Convert movie titles into emoji.\n\nBack to the Future:",
-            // prompt: 
-            //     `${prompt}`
-            temperature: 0.8,
-            max_tokens: 60,
-            top_p: 1.0,
-            frequency_penalty: 0.0,
-            presence_penalty: 0.0,
-            stop: ["\n"],
-          });
-          return await res.status(200).json({
-            sucess: true,
-            data: response.data.choices[0].text
-          });
-    } catch (error) {
-        
-    }
-})
+    //   messages=[
+    //       {"role": "system", "content": "You are a helpful assistant."},
+    //       {"role": "user", "content": "Who won the world series in 2020?"}
+    //   ])
 
-app.post("/friendly-chat", async(req,res)=>{
-    const{prompt} = req.body;
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `${prompt}`,
-        temperature: 0.5,
-        max_tokens: 60,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0,
-        stop: ["You:"],
-      });
-      return await res.status(200).json({
-        sucess: true,
-        data: response.data.choices[0].text
-      });
-      
-})
+      console.log(  response.choices[0].text);
+
+//   response = openai.ChatCompletion.create(
+//     engine=chatgpt_model_name,
+//     messages=[
+//           {"role": "system", "content": "You are a helpful assistant."},
+//           {"role": "user", "content": "Who won the world series in 2020?"}
+//       ]
+//   )
 
 
-app.post("/movie-to-emoji", async(req,res)=>{
-    try {
-        const{movie} = req.body;
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: "Convert movie titles into emoji.\n\n" + `${movie}` +":",
-            //prompt: "Convert movie titles into emoji.\n\nBack to the Future:",
-            // prompt: 
-            //     `${prompt}`
-            temperature: 0.8,
-            max_tokens: 60,
-            top_p: 1.0,
-            frequency_penalty: 0.0,
-            presence_penalty: 0.0,
-            stop: ["\n"],
-          });
-          return await res.status(200).json({
-            sucess: true,
-            data: response.data.choices[0].text
-          });
-    } catch (error) {
-        
-    }
-})
+//   for (const choice of result.choices) {
+//     console.log(choice.text);
+//   }
+//console.log(choices[0].text);
+}
 
-
-
-app.post("/find-complexity", async(req,res)=>{
-    try {
-        const{promp} = req.body;
-        const response = await openai.createCompletion({
-            model:"text-davinci-003",
-            prompt:`
-                ${prompt}
-
-                The time complexity of this funcion is ###
-            `,
-            max_tokens:64,
-            temperature:0,
-            top_p:1.0,
-            frequency_penalty:0.0,
-            presence_penalty:0.0,
-            stop:["\n"],
-        });
-        return await res.status(200).json({
-            sucess: true,
-            data: response.data.choices[0].text
-          });
-    } catch (error) {
-        return await res.status(400).json({
-            sucess: false,
-            error: error.response
-                ?error.response.data
-                :"There was an issue on the server",
-          });
-    }
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
 });
 
-
-// app.post("/find-complexity", async(req,res)=>{
-//      try {
-//         return await res.status(200).json
-//      } catch (error) {
-        
-//      }
-// });
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () =>console.log(`Server listening on port ${port}`));
+module.exports = { main };
